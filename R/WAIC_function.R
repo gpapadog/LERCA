51d54315bdf1a9379884a971eb4fa7b36e2c0c40
gpapadog <gpapadogeorgou@fas.harvard.edu>
2017-11-15 17:55:05 -0500

    WAIC using conditional likelihood

--- R/WAIC_function.R
+++ R/WAIC_function.R
@@ -9,26 +9,14 @@
 WAIC <- function(lerca, dta) {
   
   dta <- as.data.frame(dta)
+  N <- nrow(dta)
   num_conf <- dim(lerca$alphas)[5]
   covs_cols <- which((names(dta) %in% paste0('C', 1 : num_conf)))
-  N <- nrow(dta)
-  keep <- lerca$keep
+  post_samples <- dim(lerca$cutoffs)[2]
+  chains <- dim(lerca$cutoffs)[1]
   
-  # Values of mu_prior and Sigma_prior if NULL.
-  if (is.null(mu_priorX)) {
-    mu_priorX <- rep(0, num_conf + 1)
-  }
-  if (is.null(mu_priorY)) {
-    mu_priorY <- rep(0, num_conf + 2)
-  }
-  if (is.null(Sigma_priorX)) {
-    Sigma_priorX <- diag(rep(100 ^ 2, num_conf + 1))
-  }
-  if (is.null(Sigma_priorY)) {
-    Sigma_priorY <- diag(rep(100 ^ 2, num_conf + 2))
-  }
-  
-  
+  des_mat <- cbind(Int = 1, X = dta$X, dta[, cov_cols])
+
   lppd <- 0
   Elog <- 0
   logE <- 0
@@ -37,38 +25,30 @@ WAIC <- function(lerca, dta) {
   
   for (ii in 1 : N) {
     
-    log_like <- matrix(NA, nrow = length(keep), ncol = chains)
+    like <- matrix(NA, nrow = post_samples, ncol = chains)
     
-    for (ss in 1 : length(keep)) {
+    for (ss in 1 : post_samples) {
       for (cc in 1 : chains) {
         
-        curr_exper <- sum(lerca$cutoffs[cc, keep[ss], ] < dta$X[ii]) + 1
-        curr_alphaX <- lerca$alphas[cc, 1, keep[ss], curr_exper, ]
-        curr_alphaY <- lerca$alphas[cc, 2, keep[ss], curr_exper, ]
+        curr_exper <- sum(lerca$cutoffs[cc, ss, ] < dta$X[ii]) + 1
+        curr_coefsX <- lerca$coefs[1, cc, ss, curr_exper, - 2]
+        curr_coefsY <- lerca$coefs[2, cc, ss, curr_exper, ]
+        curr_sdX <- sqrt(lerca$variances[1, cc, ss, curr_exper])
+        curr_sdY <- sqrt(lerca$variances[2, cc, ss, curr_exper])
         
-        
-        log_like[ss, cc] <- CalcLogLikeXandY(D = dta[ii, ],
-                                             alphaX = curr_alphaX,
-                                             alphaY = curr_alphaY,
-                                             covs_cols = covs_cols,
-                                             nu_priorX = nu_priorX,
-                                             nu_priorY = nu_priorY,
-                                             lambda_priorX = lambda_priorX,
-                                             lambda_priorY = lambda_priorY,
-                                             mu_priorX = mu_priorX,
-                                             mu_priorY = mu_priorY, 
-                                             Sigma_priorX = Sigma_priorX,
-                                             Sigma_priorY = Sigma_priorY,
-                                             approximate = FALSE)
+        like[ss, cc] <- dnorm(x = dta$Y[ii], sd = curr_sdY,
+                              mean = sum(des_mat[ii, ] * curr_coefsY)) *
+          dnorm(x = dta$X[ii], mean = sum(des_mat[ii, - 2] * curr_coefsX),
+                sd = curr_sdX)
       }
     }
     
-    logE_ii <- log(mean(exp(log_like)))
-    Elog_ii <- mean(log_like)
+    logE_ii <- log(mean(like))
+    Elog_ii <- mean(log(like))
     
     lppd <- lppd + logE_ii
     pwaic1 <- pwaic1 + 2 * (logE_ii - Elog_ii)
-    pwaic2 <- pwaic2 + var(as.numeric(log_like))
+    pwaic2 <- pwaic2 + var(as.numeric(log(like)))
   }
   r <- c(lppd = lppd, pwaic1 = pwaic1, pwaic2 = pwaic2,
          waic1 = - 2 * (lppd - pwaic1), waic2 = - 2 * (lppd - pwaic2))
