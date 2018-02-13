4d77e2a1d69de2659c747b0c0a11f74f8c972d55
gpapadog <gpapadogeorgou@fas.harvard.edu>
2018-02-13 12:27:01 -0500

    Replacing exposure with exposure-cutoff
    For the update of the variance term in the outcome model, the design
    matrix now includes X - cutoff instead of X.

--- R/UpdateVariances_function.R
+++ R/UpdateVariances_function.R
@@ -13,21 +13,22 @@ UpdateVariances <- function(dta, current_cutoffs, current_coefs,
     
     D <- dta
     D <- subset(D, X >= cuts[ee] & X < cuts[ee + 1])
-    N_ee <- nrow(D)  # number of observations in current experiment.
+    n_k <- nrow(D)  # number of observations in current experiment.
 
     # For the exposure model.
     current_coefsX <- current_coefs[1, ee, - 2]
     resid <- D$X - cbind(1, as.matrix(D[, cov_cols])) %*% current_coefsX
     
-    alpha_new <- alpha_priorX + N_ee / 2
+    alpha_new <- alpha_priorX + n_k / 2
     beta_new <- beta_priorX + sum(resid ^ 2) / 2
     r[1, ee] <- invgamma::rinvgamma(1, shape = alpha_new, rate = beta_new)
     
     # For the outcome model.
     current_coefsY <- current_coefs[2, ee, ]
-    resid <- D$Y - cbind(1, D$X, as.matrix(D[, cov_cols])) %*% current_coefsY
+    des_mat <- cbind(1, D$X - cuts[ee], as.matrix(D[, cov_cols]))
+    resid <- D$Y - des_mat %*% current_coefsY
     
-    alpha_new <- alpha_priorY + N_ee / 2
+    alpha_new <- alpha_priorY + n_k / 2
     beta_new <- beta_priorY + sum(resid ^ 2) / 2
     r[2, ee] <- invgamma::rinvgamma(1, shape = alpha_new, rate = beta_new)
   }
