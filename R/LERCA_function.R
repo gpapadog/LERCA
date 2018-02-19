8a6baad9f4c6f0f9e19b2cb372e6bda78c77d5d6
gpapadog <gpapadogeorgou@fas.harvard.edu>
2018-02-19 18:48:40 -0500

    Revert "commit"
    This reverts commit 2f14a9ab4e1bbc6acf428d5f82f55c46bb5db1e1.

--- R/LERCA_function.R
+++ R/LERCA_function.R
@@ -24,7 +24,7 @@ LERCA <- function(dta, chains, Nsims, K, cov_cols, omega = 5000,
   prop_distribution <- match.arg(prop_distribution)
   
   num_exper <- K + 1
-  num_conf <- ifelse(is.null(cov_cols), 0, length(cov_cols))
+  num_conf <- length(cov_cols)
   minX <- min(dta$X)
   maxX <- max(dta$X)
   
@@ -50,13 +50,10 @@ LERCA <- function(dta, chains, Nsims, K, cov_cols, omega = 5000,
                        num_conf = num_conf, omega = omega, minX = minX,
                        maxX = maxX, starting_cutoffs = starting_cutoffs,
                        starting_alphas = starting_alphas)
+  alphas <- arrays$alphas
   cutoffs <- arrays$cutoffs
   coefs <- arrays$coefs
   variances <- arrays$variances
-  alphas <- NULL
-  if (num_conf > 0) {
-    alphas <- arrays$alphas
-  }
   
   acc <- array(0, dim = c(3, 2, chains))
   dimnames(acc) <- list(kind = c('separate', 'jumpOver', 'jumpWithin'),
@@ -73,7 +70,7 @@ LERCA <- function(dta, chains, Nsims, K, cov_cols, omega = 5000,
 
       current_coefs <- coefs[, cc, ii - 1, , ]
       current_vars <- variances[, cc, ii - 1, ]
-      current_alphas <- alphas[, cc, ii - 1, , ]  # NULL if no covariates.
+      current_alphas <- alphas[, cc, ii - 1, , ]
       current_cutoffs <- cutoffs[cc, ii - 1, ]
       
       # Defining experiment based on current configuration.
@@ -81,10 +78,21 @@ LERCA <- function(dta, chains, Nsims, K, cov_cols, omega = 5000,
       dta$E <- sapply(dta$X, function(x) sum(x >= cuts))
 
       
+      # ------ Updating the variances.
+      
+      var_upd <- UpdateVariances(dta = dta, current_cutoffs = current_cutoffs,
+                                 current_coefs = current_coefs,
+                                 alpha_priorX = alpha_priorX,
+                                 beta_priorX = beta_priorX,
+                                 alpha_priorY = alpha_priorY,
+                                 beta_priorY = beta_priorY)
+      variances[, cc, ii, ] <- var_upd
+      current_vars <- variances[, cc, ii, ]
+      
+      
       # ------ Updating the covariates' coefficients.
       
-      # If no covariates exist, we only update the intercepts of the exposure.
-      
+      # Set current coefficients to the previous ones.
       coefs[, cc, ii, , ] <- coefs[, cc, ii - 1, , ]
       coef_upd <- UpdateCovCoef(dta = dta, cov_cols = cov_cols,
                                 current_cutoffs = current_cutoffs,
@@ -98,32 +106,36 @@ LERCA <- function(dta, chains, Nsims, K, cov_cols, omega = 5000,
       coefs[1, cc, ii, , - 2] <- coef_upd[1 , , ]
       coefs[2, cc, ii, , - c(1, 2)] <- coef_upd[2, , - 1]
       
+      # ----- TAKE OUT --- setting equal to true values.
+      coefs[2, cc, ii, , - c(1, 2)] <- t(out_coef)
+      
       current_coefs <- coefs[, cc, ii, , ]
       
       
-      # ------ Updating the variances.
+      # ------ Updating the exposure slopes.
       
-      var_upd <- UpdateVariances(dta = dta, current_cutoffs = current_cutoffs,
+      slope_upd <- UpdateExpCoef(dta = dta, cov_cols = cov_cols,
+                                 current_cutoffs = current_cutoffs,
                                  current_coefs = current_coefs,
-                                 cov_cols = cov_cols,
-                                 alpha_priorX = alpha_priorX,
-                                 beta_priorX = beta_priorX,
-                                 alpha_priorY = alpha_priorY,
-                                 beta_priorY = beta_priorY)
-      variances[, cc, ii, ] <- var_upd
-      current_vars <- variances[, cc, ii, ]
+                                 current_vars = current_vars,
+                                 Sigma_priorY = Sigma_priorY,
+                                 mu_priorY = mu_priorY)
+      coefs[2, cc, ii, , 2] <- slope_upd
+      current_coefs <- coefs[, cc, ii, , ]
       
       
-      # ----- Updating the intercept and slope of outcome models.
+      # ------ Updating the intercepts.
       
-      int_slope_upd <- UpdateIntSlope(dta = dta, cov_cols = cov_cols,
-                                      current_cutoffs = current_cutoffs,
-                                      current_coefs = current_coefs,
-                                      current_vars = current_vars,
-                                      Sigma_priorY = Sigma_priorY,
-                                      mu_priorY = mu_priorY)
+      inter_upd <- UpdateIntercepts(dta = dta, cov_cols = cov_cols,
+                                    current_cutoffs = current_cutoffs,
+                                    current_coefs = current_coefs,
+                                    current_vars = current_vars,
+                                    Sigma_priorY = Sigma_priorY,
+                                    mu_priorY = mu_priorY)
+      coefs[2, cc, ii, , 1] <- inter_upd
+      current_coefs <- coefs[, cc, ii, , ]
       
-
+      
       
       cutoffs[cc, ii, ] <- cutoffs[cc, ii - 1, ]
       alphas[, cc, ii, , ] <- alphas[, cc, ii - 1, , ]
--- R/UpdateCovCoef_function.R
+++ R/UpdateCovCoef_function.R
@@ -2,41 +2,30 @@ UpdateCovCoef <- function(dta, cov_cols, current_cutoffs, current_coefs,
                           current_alphas, current_vars, Sigma_priorX,
                           mu_priorX, Sigma_priorY, mu_priorY) {
   
-  num_conf <- ifelse(is.null(cov_cols), 0, length(cov_cols))
+  minX <- min(dta$X)
+  maxX <- max(dta$X)
+  num_conf <- length(cov_cols)
   K <- length(current_cutoffs)
-  exact_cuts <- c(min(dta$X), current_cutoffs, max(dta$X))
   
-  if (num_conf > 0) {
-    stop('Code needs to be adapted for num_conf > 0.')
-  }
-  
-  
-  # Coefficients of the covariates including intercept (of exposure only).
+  # Coefficients of the potential covariates including intercept.
+  # Intercept is only updated for the exposure model.
   r <- array(0, dim = c(2, K + 1, num_conf + 1))
-  cov_names <- 'Int'
-  if (num_conf > 0) {
-    cov_names <- c(cov_names, paste0('C', 1 : num_conf))
-  }
-  dimnames(r) <- list(model = c('Exposure', 'Model'),
-                      exper = 1 : (K + 1), covar = cov_names)
   r[2, , 1] <- NA  # Intercept of the outcome model.
-  
+
+  exact_cuts <- c(minX, current_cutoffs, maxX)
   
   for (ee in 1 : (K + 1)) {
     
     D <- subset(dta, E == ee)
     
     # For the exposure model.
-    curr_variance <- current_vars[1, ee]
-    C <- matrix(1, nrow = nrow(D), ncol = 1)
-    which_in <- NULL
     
-    if (num_conf > 0) {
-      current_alphaX <- current_alphas[1, ee, ]
-      which_in <- which(current_alphaX == 1)
-      cov_cols_in <- cov_cols[which_in]
-      C <- cbind(C, as.matrix(D[, cov_cols_in]))
-    }
+    current_alphaX <- current_alphas[1, ee, ]
+    curr_variance <- current_vars[1, ee]
+    
+    which_in <- which(current_alphaX == 1)
+    cov_cols_in <- cov_cols[which_in]
+    C <- cbind(1, as.matrix(D[, cov_cols_in]))
     
     prior_var <- Sigma_priorX[c(1, which_in + 1), c(1, which_in + 1)]
     prior_mean <- mu_priorX[c(1, which_in + 1)]
@@ -52,32 +41,30 @@ UpdateCovCoef <- function(dta, cov_cols, current_cutoffs, current_coefs,
     r[1, ee, c(1, which_in + 1)] <- gen_coef
     
     
-    # For the outcome model, only update coefficients of covariates.
-    if (num_conf > 0) {
-      
-      current_alphaY <- current_alphas[2, ee, ]
-      curr_variance <- current_vars[2, ee]
-      
-      which_in <- which(current_alphaY == 1)
-      cov_cols_in <- cov_cols[which_in]
-      
-      # Prior of the coefficients for the covariates.
-      prior_var <- Sigma_priorY[which_in + 2, which_in + 2]
-      prior_mean <- mu_priorY[which_in + 2]
-      C <- as.matrix(D[, cov_cols_in, drop = FALSE])
-      
-      prior_var_inv <- chol2inv(chol(prior_var))
-      post_var <- t(C) %*% C / curr_variance + prior_var_inv
-      post_var <- chol2inv(chol(post_var))
-      
-      resid <- D$Y - cbind(1, D$X - exact_cuts[ee]) %*% current_coefs[2, ee, 1 : 2]
-      post_mean <- t(C) %*% matrix(resid, ncol = 1) / curr_variance
-      post_mean <- post_var %*% (post_mean + prior_var_inv %*% prior_mean)
-      
-      gen_coef <- mvnfast::rmvn(1, mu = post_mean, sigma = post_var)
-      r[2, ee, which_in + 1] <- gen_coef
-    }
+    # For the outcome model.
+    
+    current_alphaY <- current_alphas[2, ee, ]
+    curr_variance <- current_vars[2, ee]
+    
+    which_in <- which(current_alphaY == 1)
+    cov_cols_in <- cov_cols[which_in]
+    
+    # Prior of the coefficients for the covariates.
+    prior_var <- Sigma_priorY[which_in + 2, which_in + 2]
+    prior_mean <- mu_priorY[which_in + 2]
+    C <- as.matrix(D[, cov_cols_in, drop = FALSE])
+    
+    prior_var_inv <- chol2inv(chol(prior_var))
+    post_var <- t(C) %*% C / curr_variance + prior_var_inv
+    post_var <- chol2inv(chol(post_var))
+    
+    resid <- D$Y - cbind(1, D$X - exact_cuts[ee]) %*% current_coefs[2, ee, 1 : 2]
+    post_mean <- t(C) %*% matrix(resid, ncol = 1) / curr_variance
+    post_mean <- post_var %*% (post_mean + prior_var_inv %*% prior_mean)
+    
+    gen_coef <- mvnfast::rmvn(1, mu = post_mean, sigma = post_var)
+    r[2, ee, which_in + 1] <- gen_coef
+    
   }
-  
   return(r)
 }
