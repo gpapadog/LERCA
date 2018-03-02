32cf2d621baed3f2df4150a1f06373d0fb483867
gpapadog <gpapadogeorgou@fas.harvard.edu>
2018-03-02 16:18:37 -0500

    Allow for NULL coefficients
    the coefficients should be set to NULL when the update of the
    experiments is not performed with update of the slopes as well

--- R/LogLike_function.R
+++ R/LogLike_function.R
@@ -2,7 +2,8 @@
 #' 
 #' Calculating the log likelihood based on the BIC approximation. We use X
 #' instead of X - s in the outcome model since it provides the same BIC value.
-LogLike <- function(D, curr_exper_alphas, curr_coefsY, X_s_cut, cov_cols) {
+LogLike <- function(D, curr_exper_alphas, curr_coefsY = NULL, X_s_cut = 0,
+                    cov_cols) {
   
   cov_matrix <- as.matrix(D[, cov_cols])
   
@@ -14,11 +15,17 @@ LogLike <- function(D, curr_exper_alphas, curr_coefsY, X_s_cut, cov_cols) {
   
   # For the outcome model.
   des_mat <- matrix(cov_matrix[, curr_exper_alphas[2, ] == 1], nrow = nrow(D))
-  resid <- D$Y - cbind(1, D$X - X_s_cut) %*% matrix(curr_coefsY, ncol = 1)
-  if (ncol(des_mat) > 0) {
-    lmod <- lm(resid ~ des_mat - 1)
+  if (is.null(curr_coefsY)) {
+    des_mat <- cbind(1, D$X, des_mat)
+    lmod <- lm(D$Y ~ des_mat)
   } else {
-    lmod <- lm(resid ~ - 1)
+    resid <- D$Y - cbind(1, D$X - X_s_cut) %*% matrix(curr_coefsY, ncol = 1)
+    if (ncol(des_mat) > 0) {
+      lmod <- lm(resid ~ des_mat - 1)
+    } else {
+      lmod <- lm(resid ~ - 1)
+    }
+    
   }
   log_like <- log_like - BIC(lmod) / 2
   
