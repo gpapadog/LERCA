50250ab1e061b6f922c30a40d0adb4c5623d84c9
gpapadog <gpapadogeorgou@fas.harvard.edu>
2018-03-21 16:05:28 -0400

    speeding up
    Speeding up WAIC calculation by looking over the experiments instead of
    the observations

--- R/WAIC_function.R
+++ R/WAIC_function.R
@@ -18,47 +18,49 @@ WAIC <- function(lerca, dta) {
   cov_cols <- which((names(dta) %in% paste0('C', 1 : num_conf)))
   post_samples <- dim(lerca$cutoffs)[2]
   chains <- dim(lerca$cutoffs)[1]
+  K <- dim(lerca$cutoffs)[3]
   
-  des_mat <- cbind(Int = 1, X = dta$X, dta[, cov_cols])
-
-  lppd <- 0
-  Elog <- 0
-  logE <- 0
-  pwaic1 <- 0
-  pwaic2 <- 0
+  des_mat <- as.matrix(cbind(Int = 1, X = dta$X, dta[, cov_cols]))
   
-  progress <- floor(seq(0, N, length.out = 11)[- 1])
+  progress <- floor(seq(0, post_samples, length.out = 11)[- 1])
+  likelihood <- array(NA, dim = c(chains, post_samples, N))
   
-  for (ii in 1 : N) {
+  for (ss in 1 : post_samples) {
     
-    if (ii %in% progress) {
-      print(paste0(which(progress == ii) * 10, '% completed.'))
+    if (ss %in% progress) {
+      print(paste0(which(progress == ss) * 10, '% completed.'))
     }
-    like <- matrix(NA, nrow = post_samples, ncol = chains)
     
-    for (ss in 1 : post_samples) {
-      for (cc in 1 : chains) {
+    for (cc in 1 : chains) {
+      
+      curr_cutoffs <- lerca$cutoffs[cc, ss, ]
+      dta_exper <- sapply(dta$X, function(x) sum(curr_cutoffs < x) + 1)
+      
+      for (ee in 1 : (K + 1)) {
         
-        curr_exper <- sum(lerca$cutoffs[cc, ss, ] < dta$X[ii]) + 1
-        curr_coefsX <- lerca$coefs[1, cc, ss, curr_exper, - 2]
-        curr_coefsY <- lerca$coefs[2, cc, ss, curr_exper, ]
-        curr_sdX <- sqrt(lerca$variances[1, cc, ss, curr_exper])
-        curr_sdY <- sqrt(lerca$variances[2, cc, ss, curr_exper])
+        wh_obs <- which(dta_exper == ee)
+        curr_coefsX <- lerca$coefs[1, cc, ss, ee, - 2]
+        curr_coefsY <- lerca$coefs[2, cc, ss, ee, ]
+        curr_sdX <- sqrt(lerca$variances[1, cc, ss, ee])
+        curr_sdY <- sqrt(lerca$variances[2, cc, ss, ee])
+        curr_meanX <- des_mat[wh_obs, - 2] %*% curr_coefsX
+        curr_meanY <- des_mat[wh_obs, ] %*% curr_coefsY
         
-        like[ss, cc] <- dnorm(x = dta$Y[ii], sd = curr_sdY,
-                              mean = sum(des_mat[ii, ] * curr_coefsY)) *
-          dnorm(x = dta$X[ii], mean = sum(des_mat[ii, - 2] * curr_coefsX),
-                sd = curr_sdX)
+        likeY <- dnorm(x = dta$Y[wh_obs], sd = curr_sdY, mean = curr_meanY)
+        likeX <- dnorm(x = dta$X[wh_obs], sd = curr_sdX, mean = curr_meanX)
+        
+        likelihood[cc, ss, wh_obs] <- likeY * likeX
       }
     }
-    
-    logE_ii <- log(mean(like))
-    Elog_ii <- mean(log(like))
-    
-    lppd <- lppd + logE_ii
-    pwaic1 <- pwaic1 + 2 * (logE_ii - Elog_ii)
-    pwaic2 <- pwaic2 + var(as.numeric(log(like)))
   }
+  
+  logE <- log(apply(likelihood, 3, mean))
+  Elog <- apply(log(likelihood), 3, mean)
+  
+  pwaic1 <- 2 * sum(logE - Elog)
+  pwaic2 <- sum(apply(log(likelihood), 3, function(x) var(as.numeric(x))))
+  lppd <- sum(logE)
+  
   r <- c(lppd = lppd, pwaic1 = pwaic1, pwaic2 = pwaic2,
          waic1 = - 2 * (lppd - pwaic1), waic2 = - 2 * (lppd - pwaic2))
   return(r)
